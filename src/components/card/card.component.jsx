76c0e2abcb6c8fd333f46053e6b857c99b6a9f2f
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-12-11 14:36:01 -0500

    Creates SearchBox component and filters cards base on input

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,6 +6,7 @@ export const Card = (props) => (
   <div className="card-container">
     <img
       alt="monsters"
+      // monster.id used by api to generate random Monster images
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
     <h1>{props.monster.name}</h1>
