b7f9a3906877238b1fc30852d64d71a74d13d87e
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-12-11 15:18:52 -0500

    Adds final styling and code refactor

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -9,7 +9,7 @@ export const Card = (props) => (
       // monster.id used by api to generate random Monster images
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
-    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
 );
